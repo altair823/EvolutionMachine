bca658e8638ed228c5d2e980f7197b613b405c2c
altair823 <rlaxogus823@naver.com>
2022-01-23 21:44:20 +0900

    implement Environment class. implement abstracted interfaces for using Environment instance.

--- src/main/java/environment/Environment.java
+++ src/main/java/environment/Environment.java
@@ -1,6 +1,7 @@
 package environment;
 
 import machine.TypeAMachine;
+import selector.Selector;
 
 import java.io.FileNotFoundException;
 import java.nio.file.FileSystemException;
@@ -14,29 +15,99 @@ import java.util.List;
  */
 public class Environment {
     private final List<TypeAMachine> typeAMachines = new LinkedList<>();
+    private final Selector selector;
+    private final int eliminateCount;
 
     /**
      * Constructor for a new Environment object.
-     * @param inputUnitCount the number of input units.
-     * @param outputUnitCount the number of output units.
-     * @param aTypeMachineCount the number of type A machines.
-     * @param unitLayoutFilePath unit layout file path.
+     * @param inputUnitCount the number of input units
+     * @param outputUnitCount the number of output units
+     * @param aTypeMachineCount the number of type A machines
+     * @param eliminateCount the number of machines to eliminate
+     * @param selector selector instance
+     * @param unitLayoutFilePath unit layout file path
      * @throws FileSystemException wrong ULF file
      * @throws FileNotFoundException no ULF file
      */
-    Environment(int inputUnitCount, int outputUnitCount, int aTypeMachineCount, String unitLayoutFilePath) throws FileSystemException, FileNotFoundException {
+    private Environment(int inputUnitCount,
+                        int outputUnitCount,
+                        int aTypeMachineCount,
+                        int eliminateCount,
+                        Selector selector,
+                        String unitLayoutFilePath)
+            throws FileSystemException, FileNotFoundException {
         for (int i = 0; i < aTypeMachineCount; i++){
             typeAMachines.add(new TypeAMachine(inputUnitCount, outputUnitCount, unitLayoutFilePath));
         }
+        this.selector = selector;
+        this.eliminateCount = eliminateCount;
+    }
+
+    /**
+     * Method that executes an evolve method a given number of times.
+     * @param maxIterateCount a number that repeats the evolution of machines.
+     * @return the number of trials that found the desired machine, or -1 if not found.
+     */
+    public int evolveIterate(int maxIterateCount){
+        for (int i = 0; i < maxIterateCount; i++){
+            if (this.evolve()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Method that executes a single evolution of machines.
+     * @return true if a machine that meets the condition appears, otherwise false.
+     */
+    boolean evolve(){
+        this.mutate();
+        this.pulse();
+        for (TypeAMachine typeAMachine : this.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("------------------");
+        if (this.isSatisfyExpectedValue() != null){
+            return true;
+        }
+        this.select();
+        return false;
     }
 
     /**
      * Method that makes a pulse and send it to all existing machines.
      */
-    public void pulse(){
-        for (TypeAMachine typeAMachine : typeAMachines){
+    void pulse(){
+        for (TypeAMachine typeAMachine : this.typeAMachines){
             typeAMachine.pulse();
         }
+    }
+
+    /**
+     * Method that reverses a single edge of each machine.
+     */
+    void mutate(){
+        for (TypeAMachine typeAMachine: this.typeAMachines){
+            typeAMachine.mutateEdge();
+        }
+    }
+
+    /**
+     * Method that increases the number of machines by selecting the machines closest to the desired condition.
+     * Eliminate a set number of distant machines under the desired conditions,
+     * and increase the number of nearby machines under the desired conditions by that amount.
+     */
+    void select(){
+        this.selector.select(this.typeAMachines, this.eliminateCount);
+    }
+
+    /**
+     * Method that checks whether each machine satisfies the conditions of the environment
+     * @return the machine instance that satisfies the conditions. null if not.
+     */
+    TypeAMachine isSatisfyExpectedValue(){
+        return this.selector.isExpectedValueMachineExisted(this.typeAMachines);
     }
 
     /**
@@ -45,5 +116,90 @@ public class Environment {
      */
     public List<TypeAMachine> getTypeAMachines(){
         return this.typeAMachines;
+    }
+
+    /**
+     * Builder class that create Environment class.
+     */
+    public static class EnvironmentBuilder{
+        private int inputUnitCount;
+        private int outputUnitCount;
+        private int machineCount;
+        private int eliminateCount;
+        private Selector selector;
+        private final String unitLayoutFilePath;
+
+        /**
+         * Constructor for Environment Builder.
+         * @param unitLayoutFilePath unit layout file path
+         */
+        public EnvironmentBuilder(String unitLayoutFilePath){
+            this.unitLayoutFilePath = unitLayoutFilePath;
+        }
+
+        /**
+         * Setter for inputUnitCount.
+         * @param inputUnitCount number of input units.
+         * @return Environment builder instance
+         */
+        public EnvironmentBuilder setInputUnitCount(int inputUnitCount){
+            this.inputUnitCount = inputUnitCount;
+            return this;
+        }
+
+        /**
+         * Setter for outputUnitCount.
+         * @param outputUnitCount number of output units.
+         * @return Environment builder instance
+         */
+        public EnvironmentBuilder setOutputUnitCount(int outputUnitCount){
+            this.outputUnitCount = outputUnitCount;
+            return this;
+        }
+
+        /**
+         * Setter for machine count.
+         * @param machineCount number of machines
+         * @return Environment builder instance
+         */
+        public EnvironmentBuilder setMachineCount(int machineCount){
+            this.machineCount = machineCount;
+            return this;
+        }
+
+        /**
+         * Setter for selector of environment.
+         * @param selector new selector
+         * @return Environment builder instance
+         */
+        public EnvironmentBuilder setSelector(Selector selector){
+            this.selector = selector;
+            return this;
+        }
+
+        /**
+         * Setter for count of machines to eliminate.
+         * @param eliminateCount number of machines to eliminate.
+         * @return Environment builder instance
+         */
+        public EnvironmentBuilder setEliminateCount(int eliminateCount){
+            this.eliminateCount = eliminateCount;
+            return this;
+        }
+
+        /**
+         * Method that build a new Environment instance.
+         * @return new Environment instance
+         * @throws FileSystemException wrong ULF file
+         * @throws FileNotFoundException no ULF file
+         */
+        public Environment build() throws FileSystemException, FileNotFoundException {
+            return new Environment(this.inputUnitCount,
+                    this.outputUnitCount,
+                    this.machineCount,
+                    this.eliminateCount,
+                    this.selector,
+                    this.unitLayoutFilePath);
+        }
     }
 }
--- src/test/java/environment/EnvironmentTest.java
+++ src/test/java/environment/EnvironmentTest.java
@@ -2,14 +2,22 @@ package environment;
 
 import machine.TypeAMachine;
 import org.junit.jupiter.api.Test;
+import selector.ApproximateSelector;
 
 import java.io.FileNotFoundException;
 import java.nio.file.FileSystemException;
+import java.util.BitSet;
 
 class EnvironmentTest {
     private final Environment environment;
     EnvironmentTest() throws FileSystemException, FileNotFoundException {
-        this.environment = new Environment(8, 8, 10, "/Users/altair823/IdeaProjects/EvolutionMachine/layout/machine1.ulf");
+        this.environment = new Environment.EnvironmentBuilder("/Users/altair823/IdeaProjects/EvolutionMachine/layout/machine1.ulf")
+                .setInputUnitCount(8)
+                .setOutputUnitCount(8)
+                .setMachineCount(10)
+                .setSelector(new ApproximateSelector(new BitSet(8)))
+                .setEliminateCount(5)
+                .build();
     }
 
     @Test
@@ -21,5 +29,75 @@ class EnvironmentTest {
         for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
             System.out.println(typeAMachine);
         }
+    }
+
+    @Test
+    void mutateTest(){
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        for (int i = 0; i < 50; i++) {
+            this.environment.mutate();
+        }
+        this.environment.pulse();
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+    }
+
+    @Test
+    void selectTest(){
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("------------------");
+        for (int i = 0; i < 50; i++) {
+            this.environment.mutate();
+        }
+        this.environment.pulse();
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("------------------");
+        this.environment.select();
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+    }
+
+    @Test
+    void isSatisfyExpectedValueTest(){
+
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("------------------");
+        for (int i = 0; i < 50; i++) {
+            this.environment.mutate();
+        }
+        this.environment.pulse();
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("------------------");
+        System.out.println(this.environment.isSatisfyExpectedValue());
+        this.environment.select();
+    }
+
+    @Test
+    void evolveTest(){
+        int cycleCount = 0;
+        while (!this.environment.evolve()){
+            cycleCount++;
+        }
+        for (TypeAMachine typeAMachine : this.environment.getTypeAMachines()){
+            System.out.println(typeAMachine);
+        }
+        System.out.println("cycle count: " + cycleCount);
+    }
+
+    @Test
+    void evolveIterateTest(){
+        System.out.println("cycle count: " + this.environment.evolveIterate(1000));
     }
 }
