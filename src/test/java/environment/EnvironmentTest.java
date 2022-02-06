85ab912cb4b1142d70e4cc2979630fbe6797696e
altair823 <rlaxogus823@naver.com>
2022-02-07 01:49:24 +0900

    edit gradle, add lib file

--- src/test/java/environment/EnvironmentTest.java
+++ src/test/java/environment/EnvironmentTest.java
@@ -9,6 +9,7 @@ import selector.ApproximateSelector;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystemException;
+import java.sql.*;
 import java.util.BitSet;
 
 class EnvironmentTest {
@@ -104,5 +105,35 @@ class EnvironmentTest {
     @Test
     void evolveIterateTest() throws IOException {
         System.out.println("cycle count: " + this.environment.evolveIterate(1000));
+    }
+    @Test
+    void dbTest() {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    "jdbc:mariadb://61.73.209.90:3306/unorganized_machine",
+                    "altair823",
+                    "823eric!@");
+
+            pstmt = con.prepareStatement("Select version()");
+
+            rs = pstmt.executeQuery();
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(con != null) {
+                    con.close(); // 필수 사항
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
